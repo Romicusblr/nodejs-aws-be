0223c65647d7e5105e8fe60d705ab1e8c395a176
Raman <raman.mastyka@gmail.com>
2020-11-02 14:08:19 +0300

    add error handlers

--- product-service/handlers/getProductsList.ts
+++ product-service/handlers/getProductsList.ts
@@ -3,15 +3,30 @@ import "source-map-support/register";
 // commonjs import for jest importing
 const productList = require("./productList.json");
 
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Credentials": true,
+};
+
 const getProductsList: APIGatewayProxyHandler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(productList),
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
-    },
-  };
+  try {
+    const res = {
+      statusCode: 200,
+      body: JSON.stringify(productList),
+      headers,
+    };
+    return res;
+  } catch (e) {
+    console.log("getProductsList:APIGatewayProxyHandler error", e);
+
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({
+        message: "Internal Server Error",
+      }),
+    };
+  }
 };
 
 export default getProductsList;
