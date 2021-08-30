7067ec635315464e5df8db239ae9efe91b8bd9bf
Raman Mastyka <raman.mastyka@gmail.com>
2021-08-31 00:52:19 +0200

    update error solving

--- product-service/README.md
+++ product-service/README.md
@@ -9,7 +9,7 @@
 - [x] Async/await is used in lambda functions
 - [x] ES6 modules are used for product-service implementation
 - [x] Webpack is configured for product-service
-- [ ] (All languages) - SWAGGER documentation is created for product-service
+- [x] (All languages) - SWAGGER documentation is created for product-service
 - [x] (All languages) - Lambda handlers are covered by basic UNIT tests (NO infrastructure logic is needed to be covered)
 - [x] (All languages) - Lambda handlers (getProductsList, getProductsById) code is written not in 1 single module (file) and separated in codebase.
-- [ ] (All languages) - Main error scenarious are handled by API ("Product not found" error).
+- [x] (All languages) - Main error scenarious are handled by API ("Product not found" error).
--- product-service/src/functions/getProductById/handler.ts
+++ product-service/src/functions/getProductById/handler.ts
@@ -11,15 +11,19 @@ import createError from "http-errors";
 const handler: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
   event
 ) => {
-  const productId = event?.pathParameters?.productId;
+  try {
+    const productId = event?.pathParameters?.productId;
 
-  const product = productList.find(({ id }) => id === productId);
+    const product = productList.find(({ id }) => id === productId);
 
-  if (!product) {
-    throw createError(404, `Product with id ${productId} is not found`);
+    if (!product) {
+      return formatJSONResponse(createError(404, `Product with id ${productId} is not found`));
+    }
+
+    return formatJSONResponse({ product });
+  } catch (err) {
+    return formatJSONResponse(createError(500, err.message));
   }
-
-  return formatJSONResponse({ product });
 };
 
 export const main = middyfy(handler);
