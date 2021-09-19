66828a28bea74974c56051ebf74c00a0e63fc2c5
Raman Mastyka <raman.mastyka@gmail.com>
2021-09-19 09:30:09 +0200

    update constraints

--- product-service/migrations/20210901215012_init.ts
+++ product-service/migrations/20210901215012_init.ts
@@ -8,15 +8,15 @@ export async function up(knex: Knex): Promise<void> {
 
   CREATE TABLE products (
     id uuid primary key default uuid_generate_v4(),
-    title text not null,
-    description text,
-    price integer check (price > 0)
+    title text not null check (char_length(title) <= 64),
+    description text check (char_length(description) <= 255),
+    price integer check (price >= 0)
   );
 
   CREATE TABLE stocks (
     id uuid primary key default uuid_generate_v4(),
-    product_id uuid references products (id) on delete cascade on update cascade,
-    count integer check (count > 0)
+    product_id uuid not null references products (id) on delete cascade on update cascade,
+    count integer check (count >= 0)
   );
   `;
   await knex.raw(sql);
