2167d1fae78aace114586c5a3fffe80becc0194c
Raman Mastyka <raman.mastyka@gmail.com>
2021-10-04 10:27:36 +0200

    prettify

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -55,7 +55,7 @@ const serverlessConfiguration: AWS = {
               AllowedHeaders: ["*"],
               AllowedMethods: ["PUT"],
               AllowedOrigins: ["*"],
-            }
+            },
           ],
         },
       },
