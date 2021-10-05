569ed31215c5e01fce538fa0187fcbacd66cb90c
Raman Mastyka <raman.mastyka@gmail.com>
2021-10-05 15:10:23 +0200

    add creating product

--- product-service/src/functions/catalogBatchProcess/handler.ts
+++ product-service/src/functions/catalogBatchProcess/handler.ts
@@ -3,17 +3,22 @@ import "source-map-support/register";
 import type { SQSEvent, Handler } from "aws-lambda";
 // import { middyfy } from "@libs/lambda";
 import DBTransactionDAO from "@daos/db_transaction";
-
+import { StocksDBInitializer, ProductsDBInitializer } from "@models/db";
 // import schema from "./schema";
+
+type Data = StocksDBInitializer & ProductsDBInitializer;
 
 const handler: Handler<SQSEvent> = async (event) => {
   let dao: DBTransactionDAO;
   try {
     dao = new DBTransactionDAO();
     for (const record of event.Records) {
-      console.dir({ record });
+      console.log("record", record);
+      const data: Data = JSON.parse(record.body);
+      console.log("data", data);
+      const productCreated = await dao.createProduct(data);
+      console.log("productCreated", productCreated);
     }
-    // const productList = await dao.createProduct(product);
     return true;
   } catch (err) {
     console.log(err);
