90ebd0d7c62c098afc22ff8a0327ce4ac87bba92
Raman Mastyka <raman.mastyka@gmail.com>
2021-10-05 11:23:06 +0200

    add: catalogBatchProcess function, catalogItemsQueue queue

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -6,6 +6,7 @@ import {
   getProductById,
   getProductsList,
   createProduct,
+  catalogBatchProcess
 } from "@functions/index";
 
 const serverlessConfiguration: AWS = {
@@ -34,7 +35,45 @@ const serverlessConfiguration: AWS = {
     lambdaHashingVersion: "20201221",
   },
   // import the function via paths
-  functions: { getProductById, getProductsList, createProduct },
+  functions: { getProductById, getProductsList, createProduct, catalogBatchProcess },
+  resources: {
+    Resources: {
+      uploadQueueDead: {
+        Type: "AWS::SQS::Queue",
+        Properties: {
+          QueueName: process.env.UPLOAD_QUEUE_NAME + "Dead",
+        },
+      },
+      uploadQueue: {
+        Type: "AWS::SQS::Queue",
+        Properties: {
+          QueueName: process.env.UPLOAD_QUEUE_NAME,
+          RedrivePolicy: {
+            deadLetterTargetArn: { "Fn::GetAtt": ["uploadQueueDead", "Arn"] },
+            maxReceiveCount: 3,
+          },
+        },
+      },
+    },
+    Outputs: {
+      UploadQueueURL: {
+        Description: "URL of the upload queue",
+        Value: { Ref: "uploadQueue" },
+      },
+      SourceQueueARN: {
+        Description: "ARN of source queue",
+        Value: { "Fn::GetAtt": ["uploadQueue", "Arn"] },
+      },
+      DeadLetterQueueURL: {
+        Description: "URL of dead-letter queue",
+        Value: { Ref: "uploadQueueDead" },
+      },
+      DeadLetterQueueARN: {
+        Description: "ARN of dead-letter queue",
+        Value: { "Fn::GetAtt": ["uploadQueueDead", "Arn"] },
+      },
+    },
+  },
 };
 
 module.exports = serverlessConfiguration;
